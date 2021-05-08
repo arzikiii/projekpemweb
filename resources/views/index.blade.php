7fd0a067db4459513281ff31eb2a106f9a14ff44
arzikiii <arzi1605@gmail.com>
2021-05-09 00:59:56 +0900

    Halaman Utama + model & controller

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
-@extends(layout.main)
+@extends('layout/main')
 
 @section('title', 'Halaman Utama')
 
 @section('container')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col">
-        <h1 class="mt-3">Halaman Utama</h1>
+            <h1 class="mt-3">Halaman Utama</h1>
         </div>
     </div>
 </div>
